98359b0f841dbff07299b6251e5a70ff7a7280b1
Sachin Ahuja <sachinahj@gmail.com>
2016-09-02 16:19:47 -0500

    cache false

--- riskfactor-app/www/riskfactor/routes.js
+++ riskfactor-app/www/riskfactor/routes.js
@@ -2,47 +2,50 @@ riskfactorApp.config(function ($stateProvider, $urlRouterProvider, $ionicConfigP
 
   $stateProvider
     .state('splash', {
+      cache: false,
       url: '/splash',
       templateUrl: 'riskfactor/splash/splash.html',
       controller: 'SplashController'
     })
     .state('about', {
+      cache: false,
       url: '/about',
       templateUrl: 'riskfactor/splash/about.html',
       controller: 'AboutController'
     })
     .state('status', {
+      cache: false,
       url: '/status',
       templateUrl: 'riskfactor/splash/status.html',
       controller: 'StatusController',
       params: {'type': true}
     })
     .state('login', {
+      cache: false,
       url: '/login',
       templateUrl: 'riskfactor/auth/login.html',
       controller: 'LoginController'
     })
-    // .state('loginfacebook', {
-    //   url: '/loginfacebook',
-    //   templateUrl: 'riskfactor/auth/loginfacebook.html',
-    //   controller: 'LoginFacebookController'
-    // })
     .state('registration', {
+      cache: false,
       url: '/registration',
       templateUrl: 'riskfactor/auth/registration.html',
       controller: 'RegistrationController'
     })
     .state('questions', {
+      cache: false,
       url: '/questions',
       templateUrl: 'riskfactor/questions/questions.html',
       controller: 'QuestionsController'
     })
     .state('results', {
+      cache: false,
       url: '/results',
       templateUrl: 'riskfactor/results/results.html',
       controller: 'ResultsController'
     })
     .state('contact', {
+      cache: false,
       url: '/contact',
       templateUrl: 'riskfactor/splash/contact.html',
       controller: 'ContactController'
