a1dcd6d44153a53dbbe38bca192acf6072e2cd1c
Sachin Ahuja <sachinahj@gmail.com>
2017-04-25 20:44:55 -0500

    fix feedback

--- riskfactor-app/www/riskfactor/splash/contact.controller.js
+++ riskfactor-app/www/riskfactor/splash/contact.controller.js
@@ -4,18 +4,30 @@ riskfactorApp.controller('ContactController', function ($scope, $state, $cordova
 
   $scope.sendFeedback = function (feedbackText) {
     console.log("feedbackText", feedbackText);
-    if (feedbackText) {
-      $cordovaSocialSharing
-        .shareViaEmail(feedbackText, 'uThought Feedback', ['team@riskfactor.info'], [], [], [])
-        .then(function (result) {
-          // Success!
-          $scope.feedbackSent = true;
-        }, function (err) {
-          // An error occurred. Show a message to the user
-        });
+    if (feedbackText && window.cordova) {
+      window.plugins.socialsharing
+        .shareViaEmail(
+          feedbackText,
+          'uThought Feedback',
+          ['team@riskfactor.info'],
+          [],
+          [],
+          [],
+          function (result) {
+            // Success!
+            console.log("shareViaEmail | result", result);
+            $scope.feedbackSent = true;
+          },
+          function (err) {
+            // An error occurred. Show a message to the user
+            console.log("shareViaEmail | err", err);
+            $scope.feedbackSent = true;
+          }
+        )
 
       return true;
     } else {
+      $scope.feedbackSent = true;
       return false;
     }
   };
